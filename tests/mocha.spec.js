57cd8654c9af9fdcb3cd33960be6c25eea983a7d
Stefano Ramponi <stefano.ramponi.lav@gmail.com>
2023-02-25 15:07:46 +0100

    first mocha commit

---
+++ tests/mocha.spec.js
@@ -0,0 +1,26 @@
+const { chromium } = require('playwright')
+const expect = require('chai').expect
+
+let browser
+let page
+
+before(async () => {
+    browser = await chromium.launch();
+})
+
+after(async () => {
+    await browser.close();
+})
+
+beforeEach(async () => {
+    page = await browser.newPage();
+})
+
+afterEach(async () => {
+    await page.close();
+})
+
+it('First mocha test', async () => {
+    await page.goto('https://www.exmple.com')
+    expect('h1').to.exist
+})
